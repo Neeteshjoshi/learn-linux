bbdbaf7acea8019448e9a815a1aad76c7ba99cfd
Nitesh Joshi <neeteshjoshi2021@gmail.com>
2025-09-05 23:00:17 +0545

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">
+        Tailwind v3 Works 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
